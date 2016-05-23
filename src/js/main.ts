31ad67274529a33d6e3ea426fb9c840c91ca366d
hansschenker <gian.schenker@gmail.com>
2016-05-23 21:11:39 +0200

    load json into list

--- src/js/main.ts
+++ src/js/main.ts
@@ -6,14 +6,20 @@ const notes: Note[] = [];
 loadListFromJson();
 
 
-function loadListFromJson(){
-    
-    const note = new Note('note 3');
-    
-    notes.push(note);
-    $('#notes').append(`<li>${note.title}</li>`)
+function loadListFromJson() {
+
+    //const note = new Note('note 3');
+
+    $.getJSON('notes.json', function (data) {
+        console.dir(data.notes);
+        console.log(typeof data.notes);
+        data.notes.forEach(note => {
+            $('#notes').append(`<li class="${note.id}">${note.title}</li>`);
+        });
+    });
+
 }
 
-function loadArrayFromJson(){
-    
+function loadArrayFromJson() {
+
 }
